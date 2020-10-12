d06ed829e99d02e9c73b2ce1027643f95ca1cb66
20210 <20210@DESKTOP-FHL315I>
2020-10-12 17:59:28 +0900

    javaapp1005

---
+++ javaapp1005/src/javaapp1005/first.java
@@ -0,0 +1,11 @@
+package javaapp1005;
+
+public class first {					
+	public static void main(String[] args) {
+		System.out.println("첫 번째 자바");
+		System.out.println("????");
+		System.out.println("처음");
+		
+	}
+
+}
